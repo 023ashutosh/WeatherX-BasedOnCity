7d9af97e5e5746523414adfc4d126f2ba2c31838
Ashutosh Pratap Singh <023.ashutosh@gmail.com>
2023-12-20 20:29:25 +0530

    1.3

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let cityName = document.getElementById("cityName")
 let searchBtn =document.getElementById("searchBtn")
 let weatherContainer = document.getElementById("weatherContainer")
 
-const apiKey = `xyz`;
+const weatherIcon = getElementById("weather-icon") 
+
+const apiKey = `7cf0624eeabfe0564fd0b4d8cf24a1af`;
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?&units=metric`;
 
 searchBtn.addEventListener("click", function() {
@@ -44,9 +46,28 @@ function handleWeatherData(data) {
   const temperature = data.main.temp;
   const humidity = data.main.humidity;
   const windSpeed = data.wind.speed;
-  weatherContainer.innerHTML = `<p>City: ${cityName}</p>
-                                <p>Temperature: ${temperature}°C</p>
-                                <p>Description: ${description}</p>
-                                <p>Humidity: ${humidity}</p>
-                                <p>Wind Speed: ${windSpeed} Kms/Hr</p>`;
+  weatherContainer.innerHTML = `
+    <p>City: ${cityName}</p>
+    <p>Temperature: ${temperature}°C</p>
+    <p>Description: ${description}</p>
+    <p>Humidity: ${humidity}</p>
+    <p>Wind Speed: ${windSpeed} Kms/Hr</p>
+  `;
+  if(data.weather[0].main == "Clouds"){
+    weatherIcon.src="images/clouds.png";
+  }
+  else if(data.weather[0].main == "Clear"){
+    weatherIcon.src="images/clear.png";
+  }
+  else if(data.weather[0].main == "Rain"){
+    weatherIcon.src="images/rain.png";
+  }
+  else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src="images/drizzle.png";
+  }
+  else if(data.weather[0].main == "Mist"){
+    weatherIcon.src="images/mist.png";
+  }
+
+          
 }
